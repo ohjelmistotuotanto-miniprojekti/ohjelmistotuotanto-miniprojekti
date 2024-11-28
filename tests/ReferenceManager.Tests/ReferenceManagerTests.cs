a1c85297bdd2dc53e4bf2144fce59834bd1b75ad
TKUIL <TKUIL@protonmail.com>
2024-11-28 19:56:42 +0200

    ci/cd test

--- tests/ReferenceManager.Tests/ReferenceManagerTests.cs
+++ tests/ReferenceManager.Tests/ReferenceManagerTests.cs
@@ -1,10 +1,13 @@
 ﻿namespace ReferenceManager.Tests;
+using Xunit;
 
 public class UnitTest1
 {
     [Fact]
+    // For the CI/CD test
     public void Test1()
     {
-
+        int result = 2 + 2;
+        Assert.Equal(4, result);
     }
 }
