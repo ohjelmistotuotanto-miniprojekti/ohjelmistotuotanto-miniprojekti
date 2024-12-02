396a9fb2a4e730ae5b6a4775f22d49f5bdb7cd10
jashokuu <jenni.as.hokkanen@student.jyu.fi>
2024-12-02 14:23:42 +0200

    test to toBibtex

--- tests/ReferenceManager.Tests/ReferenceTests.cs
+++ tests/ReferenceManager.Tests/ReferenceTests.cs
@@ -1,7 +1,8 @@
+using ArticleReference = ReferenceManager.ArticleReference;
 namespace Reference.Tests;
 using Xunit;
 
-public class UnitTest1
+public class UnitTest2
 {
     [Fact]
     // For the CI/CD test
@@ -9,5 +10,31 @@ public class UnitTest1
     {
         int result = 2 + 2;
         Assert.Equal(4, result);
+    }
+
+    [Fact]
+    public void TestToBibtex()
+    {
+        var testReference = new ArticleReference
+        {
+            Key = "CBH91",
+            Author = "Allan Collins and John Seely Brown and Ann Holum",
+            Title = "Cognitive apprenticeship: making thinking visible",
+            Journal = "American Educator",
+            Year = "1991",
+            Volume = "6",
+            Pages = "38--46"
+        };
+        string result = testReference.ToBibtex();
+        Assert.Equal(
+            $"@article{{CBH91,\n" +
+            $"  author = {{Allan Collins and John Seely Brown and Ann Holum}},\n" +
+            $"  title = {{Cognitive apprenticeship: making thinking visible}},\n" +
+            $"  journal = {{American Educator}},\n" +
+            $"  year = {{1991}},\n" +
+            $"  volume = {{6}},\n" +
+            $"  pages = {{38--46}}\n" +
+            $"}}",
+            result);
     }
 }
